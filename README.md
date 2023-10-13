d4c1e48af652129213b824844a24e0b19c5dce4c
VinnyXP <viph0009@gmail.com>
2023-10-13 11:09:19 -0700

    Step 3 and 4, completed

--- README.md
+++ README.md
@@ -16,19 +16,39 @@ Language - Go
 
 
 ## Step 1
-- What should be done?
-    - [X] Read the number of bytes in a file
-    - [X] Output the number of bytes in the file
-    - [X] Add a "-c" CLI option
-    - [X] Read the input from the CLI ("test.txt")
+What should be done?
+- [X] Read the number of bytes in a file
+- [X] Output the number of bytes in the file
+- [X] Add a "-c" CLI option
+- [X] Read the input from the CLI ("test.txt")
 
-- What did I learn?
-    - Assignment operator, for loop, packages, opening/closing files in Go
-    - CLI arguments
-    - CLI flags
+What did I learn?
+- Assignment operator, for loop, packages, opening/closing files in Go
+- CLI arguments
+- CLI flags
 
 ## Step 2
-- What should be done?
-    - [X] Add a "-l" CLI option
-    - [X] Output the number of lines in the file
+What should be done?
+- [X] Add a "-l" CLI option
+- [X] Output the number of lines in the file
+
+What did I learn?
+- How to use bufio to scan a file line by line
+
+## Step 3
+What should be done?
+- [X] Add a "-w" CLI option
+- [X] Output the number of words in the file
+
+What did I learn?
+- ScanWords in bufio can be used to read a file word by word
+
+## Step 4
+What should be done?
+- [X] Add a "-m" CLI option
+- [X] Output the number of characters in the file
+
+What did I learn?
+- ScanRunes in bufio can be used to read a file character by character
+
 
--- ccwc.go
+++ ccwc.go
@@ -54,19 +54,81 @@ func l_flag(textFile string) string {
 	return strconv.Itoa(count) + " " + textFile
 }
 
+func w_flag(textFile string) string{
+	if textFile == "" {
+		return "Error: No file path provided"
+	}
+
+	MyFile, err := os.Open(textFile)
+
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		return ""
+	}
+
+	defer MyFile.Close()
+
+	scanner := bufio.NewScanner(MyFile)
+	scanner.Split(bufio.ScanWords)
+
+	var count int
+	for scanner.Scan() {
+		count++
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return strconv.Itoa(count) + " " + textFile
+}
+
+func m_flag(textFile string) string{
+	if textFile == "" {
+		return "Error: No file path provided"
+	}
+
+	MyFile, err := os.Open(textFile)
+
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		return ""
+	}
+
+	defer MyFile.Close()
+
+	scanner := bufio.NewScanner(MyFile)
+	scanner.Split(bufio.ScanRunes)
+
+	var count int
+	for scanner.Scan() {
+		count++
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return strconv.Itoa(count) + " " + textFile
+}
+
 func main() {
 	//cli options
 	strC := flag.String("c", "", "number of bytes") 
 	strL := flag.String("l", "", "number of lines")
+	strW := flag.String("w", "", "number of words")
+	strM := flag.String("m", "", "number of character")
 	flag.Parse()
 
-	if *strC != "" && *strL != "" {
+	if *strC != "" && *strL != "" && *strW != ""{
 		//If the user uses more than one flag
-		fmt.Println("Error: Please provide only one of the -c or -l flags.")
+		fmt.Println("Error: Please provide only one of the flags: -c, - l, -w, or -m.")
 	} else if *strC != "" {
 		fmt.Println(c_flag(*strC))
 	} else if *strL != "" {
 		fmt.Println(l_flag(*strL))
+	} else if *strW != "" {
+		fmt.Println(w_flag(*strW))
+	} else if *strM != "" {
+		fmt.Println(m_flag(*strM))
 	} else {
 		//If no flags were given
 		fmt.Println("Error: No valid flag value provided")
